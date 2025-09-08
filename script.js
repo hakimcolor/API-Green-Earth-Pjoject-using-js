cdd8834a678fcc6072826bc19019b182b2e28cb7
hakimcolor <hakimcolor777@gmail.com>
2025-09-08 19:47:02 +0600

    more Responsive

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const loadCategory = () => {
       const a = document.createElement('a');
       a.href = '#';
       a.className =
-        'block text-gray-800 hover:bg-green-700 cursor-pointer hover:text-white px-3 py-2 w-[10rem] rounded';
+        'block text-gray-800 hover:bg-green-700 cursor-pointer hover:text-white px-3 py-2 rounded';
       a.textContent = liName;
 
       a.addEventListener('click', (e) => {
@@ -71,11 +71,55 @@ const lodcardByCategory = (cardId) => {
 const displaycard = (cards) => {
   // console.log(cards);
   
-  cards.forEach((card) => {
-    cardbox.innerHTML += `<div>\
-    <img src="${card.image}
-">    <h1>${card.price}</h1>
-    </div>`;
+  cards.forEach((plant) => {
+    cardbox.innerHTML += `
+    <div class="w-80 bg-white shadow-md rounded-2xl overflow-hidden border border-gray-100 mt-5">
+ 
+  <div class="bg-gray-100 h-40 flex items-center justify-center">
+    <img 
+      src="${plant.image}" 
+      alt="${plant.name}" 
+      class="w-full h-full object-cover  rounded-t-2xl p-2 bg-white"
+    />
+  </div>
+
+  <!-- Content -->
+  <div class="p-4">
+    <!-- Title -->
+    <h2 onclick="loadSinglePlant('${plant.id}')" 
+        class="text-lg font-semibold text-gray-900">
+      ${plant.name}
+    </h2>
+
+    <!-- Description -->
+    <p class="text-sm text-gray-600 mt-1">
+      ${
+        plant.description
+          ? plant.description.slice(0, 100) + '...'
+          : 'No description available.'
+      }
+    </p>
+
+    
+    <div class="flex items-center justify-between mt-3">
+      <span class="px-3 py-1 text-sm rounded-full bg-green-100 text-green-700">
+        ${plant.category}
+      </span>
+      <span class="text-lg font-bold text-gray-900"><span class='text-3xl font-bold'>৳</span>${
+        plant.price
+      }</span>
+    </div>
+
+    <!-- Add to Cart -->
+    <button onclick="addToCart('${plant.id}')"
+      class="w-full bg-green-700 hover:bg-green-800 transition text-white font-medium rounded-full py-2 mt-4">
+      Add to Cart
+    </button>
+  </div>
+</div>
+
+
+      `;
     
   });
 };
